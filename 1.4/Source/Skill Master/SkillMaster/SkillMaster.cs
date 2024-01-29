04b38101a6de919224a1abadf675854e0b098f9d
Ruy-u <119630026+Ruy-u@users.noreply.github.com>
2024-01-29 19:19:14 +0300

    Fluffy worktab patch SkillMaster.cs

--- 1.4/Source/Skill Master/SkillMaster/SkillMaster.cs
+++ 1.4/Source/Skill Master/SkillMaster/SkillMaster.cs
@@ -348,6 +348,7 @@ namespace Ruyu.SkillMaster
         {
             Harmony harmony = new Harmony("Ruyu.SkillsOfColony");
             harmony.PatchAll();
+            //My thanks to divineDerivative for the worktab patch
             if (ModsConfig.IsActive("Fluffy.WorkTab"))
             {
                 harmony.Patch(AccessTools.TypeByName("WorkTab.MainTabWindow_WorkTab").GetMethod("DoWindowContents"), postfix: new HarmonyMethod(typeof(Worktab_Patch), nameof(Worktab_Patch.Postfix)));
